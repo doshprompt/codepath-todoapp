8d51aae16af450382da0986ac3ce4031ff1eebaa
Rahul Doshi <rdoshi@netflix.com>
2017-02-24 12:42:23 -0800

    allow long and short press on list items in search activity

--- app/src/main/java/rdoshi/codepath/todoapp/SearchResultsActivity.java
+++ app/src/main/java/rdoshi/codepath/todoapp/SearchResultsActivity.java
@@ -3,10 +3,13 @@ package rdoshi.codepath.todoapp;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
@@ -53,10 +56,43 @@ public class SearchResultsActivity extends AppCompatActivity {
 
             if (items.size() > 0) {
                 findViewById(R.id.lblNotFound).setVisibility(View.GONE);
+                setupListViewListener();
             } else {
                 lvItems.setVisibility(View.GONE);
             }
         }
     }
 
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                TaskItem taskItem = items.get(pos);
+
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+
+                Toast.makeText(getApplicationContext(), String.format(getString(R.string.task_complete), taskItem.getName()), Toast.LENGTH_SHORT).show();
+
+                taskItem.delete();
+
+                if (items.size() == 0) {
+                    findViewById(R.id.lblNotFound).setVisibility(View.VISIBLE);
+                    lvItems.setVisibility(View.GONE);
+                }
+
+                return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+                FragmentManager fm = getSupportFragmentManager();
+                EditItemDialogFragment editItemDialogFragment = EditItemDialogFragment
+                        .newInstance(items.get(pos), pos);
+                editItemDialogFragment.show(fm, "fragment_edit_item");
+            }
+        });
+    }
 }
